c9537b5645d3d5e5fe6fdbaaca8b761f2861ccd7
Dukenfox <68194973+Dukenfox@users.noreply.github.com>
2022-02-12 22:12:19 +0100

    seed database
    seeded database

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelpCampDb', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -9,3 +9,11 @@ mongoose.connect('mongodb://localhost:27017/yelpCampDb', { useNewUrlParser: true
         console.log("Error, MONGO CONNECTION!!!!")
         console.log(err)
     })
+
+    const seedDb = async () => {
+        await Campground.deleteMany({});
+        const c = new Campground({ title: "purple feild " });
+        await c.save(); 
+    }
+    
+    seedDb(); 
