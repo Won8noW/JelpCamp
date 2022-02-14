9b8c73acf75885c4528283f8ae0fd3853cc9f98c
Dukenfox <68194973+Dukenfox@users.noreply.github.com>
2022-02-14 17:48:03 +0100

    Update index.js
    added database of sample locations

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const cities = require('./cities');
 const Campground = require('../models/campground');
+const { descriptors, places } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelpCampDb', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -10,10 +12,17 @@ mongoose.connect('mongodb://localhost:27017/yelpCampDb', { useNewUrlParser: true
         console.log(err)
     })
 
-    const seedDb = async () => {
+    const sample = array => array[Math.floor(Math.random() * array.length)];
+
+    const seedDB = async () => {
         await Campground.deleteMany({});
-        const c = new Campground({ title: "purple feild " });
-        await c.save(); 
+        for (let i = 0; i < 50; i++) {
+            const random1000 = Math.floor(Math.random() * 1000);
+            const camp = new Campground({
+                location: `${cities[random1000].city}, ${cities[random1000].state}`,
+                title: `${sample(descriptors)} ${sample(places)}`
+            })
+            await camp.save();
+        }
     }
-    
-    seedDb(); 
+seedDB(); 
