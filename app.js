1c175407f0a3336fe10f91b6197f87100e6110dd
Dukenfox <68194973+Dukenfox@users.noreply.github.com>
2022-02-23 21:49:57 +0100

    first page

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ app.get('/', (req,res) => {
     res.render('home')
 })
 
-app.get('/campground', async (req, res) => {
-    const camp = new Campground({ title: "My Background"})
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req,res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
 })
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -25,4 +25,6 @@ mongoose.connect('mongodb://localhost:27017/yelpCampDb', { useNewUrlParser: true
             await camp.save();
         }
     }
-seedDB(); 
+seedDB().then(() => {
+    mongoose.connection.close();
+}); 
